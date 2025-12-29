7da749461cd5f1c301cfa910adb9b164513dfe75
anminfang <anminfang@tamu.edu>
2025-12-29 12:07:51 -0600

    created encoder

---
+++ src/protocol/encode.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <cstddef>
+#include <cstring>
+#include "md_message.hpp"
+
+namespace protocol
+{
+    constexpr std::size_t kWireSize = 40;
+
+// make sure match hardware arch
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    inline uint32_t host_to_be32(uint32_t v) { return __builtin_bswap32(v); }
+    inline uint64_t host_to_be64(uint64_t v) { return __builtin_bswap64(v); }
+    inline uint32_t be32_to_host(uint32_t v) { return __builtin_bswap32(v); }
+    inline uint32_t be64_to_host(uint64_t v) { return __builtin_bswap64(v); }
+#else
+    inline uint32_t host_to_be32(uint32_t v) { return v; }
+    inline uint64_t host_to_be64(uint64_t v) { return v; }
+    inline uint32_t be32_to_host(uint32_t v) { return v; }
+    inline uint64_t be64_to_host(uint64_t v) { return v; }
+#endif
+
+    // network strict size on uint8_t -> 8 bits
+    inline void write_u32(std::array<uint8_t, kWireSize> &buff, std::size_t &off, uint32_t v)
+    {
+        uint32_t be = host_to_be32(v);
+        std::memcpy(buff.data() + off, &be, sizeof(be));
+        off += sizeof(be); // jump 4 * 8 bits or 8 * 8 bits
+    }
+
+    // network strict size on uint8_t -> 8 bits
+    inline void write_i64(std::array<uint8_t, kWireSize> &buff, std::size_t &off, int64_t v)
+    {
+        uint64_t be = host_to_be64(static_cast<uint64_t>(v));
+        std::memcpy(buff.data() + off, &be, sizeof(be));
+        off += sizeof(be); // jump 4 * 8 bits or 8 * 8 bits
+    }
+
+    inline std::array<uint8_t, kWireSize> encode(const MarketDataMsg &msg)
+    {
+        // std::array<uint8_t, kWireSize> out{}; -> value init: write each bit to be 0, slow
+        // default init, fast -> avoid double writing each bit
+        std::array<uint8_t, kWireSize> out;
+        std::size_t off = 0;
+
+        write_u32(out, off, msg.symbol_id);
+        write_i64(out, off, msg.exchange_ts);
+        write_i64(out, off, msg.bid_price);
+        write_i64(out, off, msg.ask_price);
+        write_u32(out, off, msg.bid_size);
+        write_u32(out, off, msg.ask_size);
+
+        return out;
+    }
+
+}
