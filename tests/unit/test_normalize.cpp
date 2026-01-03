3ed41adf1ded636368f9313df8e0fbfd413f8919
anminfang <anminfang@tamu.edu>
2026-01-03 15:50:46 -0600

    deleted unnecessary setup

--- tests/unit/test_normalize.cpp
+++ tests/unit/test_normalize.cpp
@@ -14,129 +14,14 @@
 #include "generator/generator.hpp"
 #include "engine/normalize/normalize.hpp"
 
-class NormalizeTest : public ::testing::Test
-{
-protected:
-    static inline std::atomic<uint16_t> port_{8888};
-    static inline std::thread server_thread_;
-    static inline std::atomic<bool> ready_{false};
-    static inline std::atomic<bool> stop_{false};
-    static inline std::vector<protocol::MarketDataMsg> received;
-    static inline std::atomic<bool> server_ok_{true};
-    static inline std::mutex mutex_;
-
-    static void SetUpTestSuite()
-    {
-        server_thread_ = std::thread([]
-                                     {
-                                         int fd = socket(AF_INET, SOCK_STREAM, 0);
-                                         if (fd < 0)
-                                             return;
-
-                                         sockaddr_in addr{};
-                                         addr.sin_family = AF_INET;
-                                         addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-                                         addr.sin_port = htons(port_.load());
-
-                                         if (bind(fd, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)) != 0)
-                                         {
-                                             close(fd);
-                                             return;
-                                         }
-
-                                         socklen_t len = sizeof(addr);
-                                         if (getsockname(fd, reinterpret_cast<sockaddr *>(&addr), &len) != 0)
-                                         {
-                                             close(fd);
-                                             return;
-                                         }
-
-                                         if (listen(fd, 1) != 0)
-                                         {
-                                             close(fd);
-                                             return;
-                                         }
-
-                                         ready_.store(true, std::memory_order_release);
-
-                                         while (!stop_.load(std::memory_order_acquire))
-                                         {
-                                            int cfd = accept(fd, nullptr, nullptr);
-                                            if (cfd < 0)
-                                            {
-                                                continue;
-                                            }
-                                            
-
-                                            std::array<uint8_t, protocol::kWireSize> buff{};
-                                            while(true)
-                                            {
-                                                ssize_t n = net::recv_exact(cfd, buff.data(), buff.size());
-                                                if (n <= 0) break;
-
-                                                protocol::MarketDataMsg msg{};
-                                                if (protocol::decode(buff.data(), buff.size(), msg)) {
-                                                    std::lock_guard<std::mutex> lock(mutex_);
-                                                    received.push_back(msg);
-                                                } else {
-                                                    server_ok_.store(false);
-                                                    break;
-                                                }
-                                            }
-                                            close(cfd);
-                                         }
-                                         close(fd); });
-
-        while (!ready_.load(std::memory_order_acquire))
-        {
-            std::this_thread::yield();
-        }
-
-        received.clear();
-    }
-
-    static void TearDownTestSuite()
-    {
-        stop_.store(true, std::memory_order_release);
-
-        int fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (fd >= 0)
-        {
-            sockaddr_in addr{};
-            addr.sin_family = AF_INET;
-            addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-            addr.sin_port = htons(port_.load(std::memory_order_acquire));
-            connect(fd, reinterpret_cast<sockaddr *>(&addr), sizeof(addr));
-            close(fd);
-        }
-
-        if (server_thread_.joinable())
-        {
-            server_thread_.join();
-        }
-    }
-};
-
-TEST_F(NormalizeTest, matchSize)
-{
-    protocol::MarketDataMsg msg{1, 123, 10, 11, 20, 30};
-    generator::Generator gen;
-    bool connected = gen.connect("127.0.0.1", port_.load());
-    gen.send(msg);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-    EXPECT_EQ(sizeof(msg), sizeof(received[0]));
-}
-
-TEST_F(NormalizeTest, notMatchSize)
+TEST(NormalizeTest, matchSize)
 {
     EXPECT_TRUE(engine::isSizeMatch(protocol::kWireSize));
     EXPECT_FALSE(engine::isSizeMatch(protocol::kWireSize - 1));
     EXPECT_FALSE(engine::isSizeMatch(protocol::kWireSize + 1));
 }
 
-TEST_F(NormalizeTest, sanityCheck)
+TEST(NormalizeTest, sanityCheck)
 {
     // normal
     protocol::MarketDataMsg msg_ask_smaller_than_bid{1, 300, 10, 9, 100, 200};
@@ -159,7 +44,7 @@ TEST_F(NormalizeTest, sanityCheck)
     EXPECT_FALSE(engine::sanityCheck(msg_negative_ask_price));
 }
 
-TEST_F(NormalizeTest, decodeMsg)
+TEST(NormalizeTest, decodeMsg)
 {
     protocol::MarketDataMsg from{1, 300, 10, 11, 100, 200};
     auto bytes = protocol::encode(from);
