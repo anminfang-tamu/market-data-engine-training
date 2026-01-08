c5c0f9298974494afeb1116c6c59e157b4ae2246
anminfang <anminfang@tamu.edu>
2026-01-08 14:39:03 -0600

    added burst

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
 #include "engine/engine.hpp"
 
 int main()
 {
-    std::cout << "======== Engine ========" << std::endl;
+    std::cout << "<======== Engine ========>" << std::endl;
 
     engine::Engine engine;
     engine.run();
--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -6,5 +6,27 @@ int main()
     std::cout << "======== Generator ========" << std::endl;
     generator::Generator gen;
     gen.connect("127.0.0.1", 8888);
-    gen.run(1000000, 0, 1);
+
+    // 10-minute soak
+    const int rate = 50'000;
+    const int duration_s = 600; // 10 minutes
+    const int count = rate * duration_s;
+    const int seed = 42;
+
+    gen.run(count, rate, seed);
+
+    // burst + normal
+    const int burst_count = 100'000;
+    const int burst_rate = 0;
+    const int burst_seed = 43;
+
+    const int normal_rate = 50'000;
+    const int normal_duration = 600; // seconds
+    const int normal_count = normal_rate * normal_duration;
+    const int normal_seed = 44;
+
+    gen.run(burst_count, burst_rate, burst_seed);
+    gen.run(normal_count, normal_rate, normal_seed);
+
+    return 0;
 }
