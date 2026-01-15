5ebf120a894f9d0359e634a5fb216d82f657107e
anminfang <anminfang@tamu.edu>
2026-01-15 11:16:21 -0600

    changed gen's duration

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -28,24 +28,24 @@ int main()
     }
 
     // 10-minute soak
-    // const int rate = 50'000;
-    // const int duration_s = 600; // 10 minutes
-    // const int count = rate * duration_s;
-    // const int seed = 42;
+    const int rate = 50'000;
+    const int duration_s = 600; // 10 minutes
+    const int count = rate * duration_s;
+    const int seed = 42;
 
-    // gen.run(count, rate, seed);
+    gen.run(count, rate, seed);
 
     // burst + normal
-    const int burst_count = 100'000;
-    const int burst_rate = 0;
-    const int burst_seed = 43;
+    // const int burst_count = 100'000;
+    // const int burst_rate = 0;
+    // const int burst_seed = 43;
 
     // const int normal_rate = 50'000;
     // const int normal_duration = 600; // seconds
     // const int normal_count = normal_rate * normal_duration;
     // const int normal_seed = 44;
 
-    gen.run(burst_count, burst_rate, burst_seed);
+    // gen.run(burst_count, burst_rate, burst_seed);
     // gen.run(normal_count, normal_rate, normal_seed);
 
     LOG_INFO("<========== END ==========>");
