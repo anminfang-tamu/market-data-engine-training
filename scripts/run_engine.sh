0e185799c799a6cb9e6bb9089df67ce491629605
anminfang <anminfang@tamu.edu>
2026-04-18 14:01:24 -0500

    changed sh

--- scripts/run_engine.sh
+++ scripts/run_engine.sh
@@ -10,4 +10,11 @@ if [[ ! -x "${ENGINE_BIN}" ]]; then
   exit 1
 fi
 
+if command -v pkg-config >/dev/null 2>&1 && pkg-config --exists libdpdk; then
+  DPDK_LIBDIR="$(pkg-config --variable=libdir libdpdk)"
+  if [[ -n "${DPDK_LIBDIR}" && -d "${DPDK_LIBDIR}" ]]; then
+    export LD_LIBRARY_PATH="${DPDK_LIBDIR}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+  fi
+fi
+
 "${ENGINE_BIN}" "$@"
