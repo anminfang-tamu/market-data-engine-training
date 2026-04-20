fa02c9a96d434e90f9e922dea8426713da526cc4
anminfang <anminfang@tamu.edu>
2026-04-20 11:32:15 -0500

    changed acquired to relaxed

--- src/engine/v3/engine.cpp
+++ src/engine/v3/engine.cpp
@@ -262,7 +262,7 @@ bool Engine::run() {
   const std::size_t burst =
       cfg_.burst_size < kMaxBurst ? cfg_.burst_size : kMaxBurst;
 
-  while (!stop_requested_.load(std::memory_order_acquire) &&
+  while (!stop_requested_.load(std::memory_order_relaxed) &&
          (cfg_.stop_signal_flag == nullptr || *cfg_.stop_signal_flag == 0)) {
     const std::size_t n = rx_source_->poll(packets, burst);
     for (std::size_t i = 0; i < n; ++i) {
@@ -278,7 +278,7 @@ bool Engine::run() {
 }
 
 void Engine::request_stop() {
-  stop_requested_.store(true, std::memory_order_release);
+  stop_requested_.store(true, std::memory_order_relaxed);
 }
 
 void Engine::process_one(const PacketView &packet) {
